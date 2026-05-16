23e2f401802492d63bf705dd48705676310ee356
Raphael Reis <reis.rrsc@outlook.com>
2026-05-16 20:47:13 -0300

    feat: add base model controller factory seed migrations

---
+++ app/Http/Controllers/TicketDetailController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTicketDetailRequest;
+use App\Http\Requests\UpdateTicketDetailRequest;
+use App\Models\TicketDetail;
+
+class TicketDetailController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTicketDetailRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TicketDetail $ticketDetail)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TicketDetail $ticketDetail)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTicketDetailRequest $request, TicketDetail $ticketDetail)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TicketDetail $ticketDetail)
+    {
+        //
+    }
+}
---
+++ database/factories/TicketDetailFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\TicketDetail;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<TicketDetail>
+ */
+class TicketDetailFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
