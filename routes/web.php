853a776b31035bf491e7609f45284527767508ce
Raphael Reis <reis.rrsc@outlook.com>
2026-05-17 00:55:19 -0300

    feat: add base routes

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,22 @@ Route::middleware('auth')->group(function () {
     Route::patch('/companies/{company}/toggle-status', [CompanyController::class, 'toggleStatus'])->name('companies.toggle-status');
     Route::delete('/companies/{company}', [CompanyController::class, 'destroy'])->name('companies.destroy');
     Route::get('/companies/{company}', [CompanyController::class, 'show'])->name('companies.show');
+
+    Route::get('/projects', [ProjectController::class, 'index'])->name('projects.index');
+    Route::get('/projects/create', [ProjectController::class, 'create'])->name('projects.create');
+    Route::post('/projects', [ProjectController::class, 'store'])->name('projects.store');
+    Route::get('/projects/{project}/edit', [ProjectController::class, 'edit'])->name('projects.edit');
+    Route::put('/projects/{project}', [ProjectController::class, 'update'])->name('projects.update');
+    Route::delete('/projects/{project}', [ProjectController::class, 'destroy'])->name('projects.destroy');
+    Route::get('/projects/{project}', [ProjectController::class, 'show'])->name('projects.show');
+
+    Route::get('/tickets', [TicketController::class, 'index'])->name('tickets.index');
+    Route::get('/tickets/create', [TicketController::class, 'create'])->name('tickets.create');
+    Route::post('/tickets', [TicketController::class, 'store'])->name('tickets.store');
+    Route::get('/tickets/{tickets}/edit', [TicketController::class, 'edit'])->name('tickets.edit');
+    Route::put('/tickets/{tickets}', [TicketController::class, 'update'])->name('tickets.update');
+    Route::delete('/tickets/{tickets}', [TicketController::class, 'destroy'])->name('tickets.destroy');
+    Route::get('/tickets/{tickets}', [TicketController::class, 'show'])->name('tickets.show');
 });
 
 require __DIR__.'/auth.php';
